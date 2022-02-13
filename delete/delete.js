345710906e00c89c8df43490434ecc1073a613cc
SarfarajSalimKhan <sarfaraz.salimkhan17@rediffmail.com>
2022-02-13 22:37:53 +0530

    initial commit

---
+++ delete/delete.js
@@ -0,0 +1,35 @@
+//import modules
+const express = require('express')
+let mongodb = require('mongodb')
+//create mongo client
+let mcl = mongodb.MongoClient
+//create router instance
+let router = express.Router()
+//import url
+let url = require("../url")
+//create rest api
+router.post("/", (req, res) => {
+    var p_id = req.body.p_id
+    let obj = {
+        "p_id": p_id
+    }
+    mcl.connect(url, (err, conn) => {
+        if (err)
+            console.log("Error in connection: ", err)
+        else {
+            let db = conn.db("nodedb")
+            db.collection("products").deleteOne(obj, (err) => {
+                if (err)
+                    res.json({'delete': 'failed'})
+                else {
+                    console.log("Data deleted")
+                    res.json({'delete': 'success'})
+                }
+            })
+            
+
+        }
+    })
+})
+//export router
+module.exports = router
---
+++ url.js
@@ -0,0 +1,2 @@
+//module.exports = "mongodb://localhost:27017"
+module.exports = "mongodb+srv://admin:root@cluster0.g6o5j.mongodb.net/nodedb?retryWrites=true&w=majority"
